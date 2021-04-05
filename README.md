4ea01b64b623c7f723be0af5a4fb63fc69e3120c
jeffamady <jeff02.jn@gmail.com>
2021-04-05 00:55:52 -0300

    I created some folders and files

---
+++ README.md
@@ -0,0 +1,1 @@
+I am working on my first React Project
---
+++ src/components/Business/Business.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const business = {
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
